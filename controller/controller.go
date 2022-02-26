7f7975ad4693a8adfd04b74c414eb016294d60fb
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-27 02:12:08 +0300

    Added controller for todolist

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,49 @@
 package controller
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"web-based-todo-list-backend/models"
+	"web-based-todo-list-backend/service"
+)
+
+type IController interface {
+	GetTodoList(c *gin.Context)
+	AddTodoList(c *gin.Context)
+}
+
+type Controller struct {
+	svc service.IService
+}
+
+func (ctl *Controller) GetTodoList(c *gin.Context) {
+	res, err := ctl.svc.GetTodoList()
+	if err != nil {
+		switch err {
+		case fmt.Errorf("database Error : db is null"):
+			c.JSON(http.StatusServiceUnavailable, gin.H{"data": res})
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func (ctl *Controller) AddTodoList(c *gin.Context) {
+	body := models.PostTodoListBody{}
+	if err := c.BindJSON(&body); err != nil {
+		return
+	}
+
+	res, err := ctl.svc.AddTodoList(body.Todo)
+	if err != nil {
+		switch err {
+		case fmt.Errorf("database Error : todo already exist"):
+			c.JSON(http.StatusServiceUnavailable, gin.H{"data": res})
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"data": res})
+}
+
+func newTodoListController(service service.IService) IController {
+	return &Controller{svc: service}
+}
