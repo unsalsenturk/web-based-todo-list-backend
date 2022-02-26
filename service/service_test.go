3a09669da4d0b8d1f6356d7508dd776d1809b06a
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-27 00:59:45 +0300

    Updated TestService_GetTodoList

--- service/service_test.go
+++ service/service_test.go
@@ -1,26 +1,48 @@
 package service
 
 import (
+	"fmt"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"web-based-todo-list-backend/mock"
 	"web-based-todo-list-backend/models"
 )
 
 func TestService_GetTodoList(t *testing.T) {
-	databaseReturn := &models.DataResponse{
-		"dummy todo": models.Todo{
-			ID:          1,
-			Description: "dummy todo",
-		},
-	}
-	ctl := gomock.NewController(t)
-	defer ctl.Finish()
-	mockDb := mock.NewMockIDatabase(ctl)
-	mockDb.
-		EXPECT().
-		GetTodoList().
-		Return(databaseReturn, nil)
+	t.Run("when GetTodoList returns data properly", func(t *testing.T) {
+		databaseReturn := &models.DataResponse{
+			"dummy todo": models.Todo{
+				ID:          1,
+				Description: "dummy todo",
+			},
+		}
+		ctl := gomock.NewController(t)
+		defer ctl.Finish()
+		mockDb := mock.NewMockIDatabase(ctl)
+		mockDb.
+			EXPECT().
+			GetTodoList().
+			Return(databaseReturn, nil)
 
-	// need service
+		svc := NewService(mockDb)
+		res, err := svc.GetTodoList()
+		assert.Equal(t, res, databaseReturn)
+		assert.Nil(t, err)
+	})
+	t.Run("when GetTodoList returns error", func(t *testing.T) {
+		error := fmt.Errorf("database Error : db is null")
+		ctl := gomock.NewController(t)
+		defer ctl.Finish()
+		mockDb := mock.NewMockIDatabase(ctl)
+		mockDb.
+			EXPECT().
+			GetTodoList().
+			Return(nil, error)
+
+		svc := NewService(mockDb)
+		res, err := svc.GetTodoList()
+		assert.Equal(t, err, error)
+		assert.Nil(t, res)
+	})
 }
