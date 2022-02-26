4f730587204be19d9383c56567daae67d3680097
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-27 01:10:03 +0300

    Added TestService_AddTodoList

--- service/service_test.go
+++ service/service_test.go
@@ -46,3 +46,41 @@ func TestService_GetTodoList(t *testing.T) {
 		assert.Nil(t, res)
 	})
 }
+func TestService_AddTodoList(t *testing.T) {
+	t.Run("when AddTodoList returns data properly", func(t *testing.T) {
+		args := "dummy todo"
+		databaseReturn := &models.Todo{
+			ID:          1,
+			Description: "dummy todo",
+		}
+
+		ctl := gomock.NewController(t)
+		defer ctl.Finish()
+		mockDb := mock.NewMockIDatabase(ctl)
+		mockDb.
+			EXPECT().
+			AddTodoList(args).
+			Return(databaseReturn, nil)
+
+		svc := NewService(mockDb)
+		res, err := svc.AddTodoList(args)
+		assert.Equal(t, res, databaseReturn)
+		assert.Nil(t, err)
+	})
+	t.Run("when AddTodoList returns error", func(t *testing.T) {
+		args := "dummy todo"
+		error := fmt.Errorf("database Error : todo already exist")
+		ctl := gomock.NewController(t)
+		defer ctl.Finish()
+		mockDb := mock.NewMockIDatabase(ctl)
+		mockDb.
+			EXPECT().
+			AddTodoList(args).
+			Return(nil, error)
+
+		svc := NewService(mockDb)
+		res, err := svc.AddTodoList(args)
+		assert.Equal(t, err, error)
+		assert.Nil(t, res)
+	})
+}
