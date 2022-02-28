e7b3342ef7e3d0384f64a7e135be028f392a5770
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-28 17:42:51 +0300

    CI/CD Pipeline Test

--- server/server.go
+++ server/server.go
@@ -37,7 +37,6 @@ func (s *Server) StartServer(port int, handler controller.IController) error {
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://10.8.8.197")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
